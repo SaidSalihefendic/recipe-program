12ae4254617f5b49fefb1a4aa6f57e8fe282edf4
Said <saidsalihefendic@gmail.com>
2016-12-18 10:41:28 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void dodaj(ofstream& Ispis);
+
+int main()
+{
+    ofstream Ispis;
+
+    dodaj(Ispis); //za unos sa stojaka u frizider
+
+    return 0;
+}
+
+void dodaj(ofstream& Ispis)
+{
+    //Funkcija koja preuzima unos od korisnika za sadrzaj frizidera nakon kupovine TREBA SKONTATI KOLICINU DA SE UNESE
+    Ispis.open("Frizider/frizider.txt");
+
+    string sastojak("Ovdje neki sastojak");
+
+    cout << "Unesite svoje sastojke u frizider! Za prekid unosa, unesite X: " << endl;
+
+    while(sastojak != "X" and sastojak != "x")
+    {
+        getline(cin, sastojak);
+
+        if(sastojak != "" and sastojak != "x" and sastojak != "X")
+            Ispis << sastojak << endl;
+    }
+
+    Ispis.close();
+
+}
